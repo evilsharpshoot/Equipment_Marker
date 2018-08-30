d087edcb85e2a5f4ffda29cbef38e7931c673b55
rdyson <ryandyson@hotmail.co.uk>
2018-08-30 11:13:44 +0100

    Version 0.1.3 - Tracks Unqiue Item Instance

--- main.lua
+++ main.lua
@@ -1,33 +1,27 @@
 eventSet = false
-ids = {}
 icons = {}
+spot = {}
 
 hooksecurefunc("ContainerFrame_Update",function(self)
-	if not eventSet then
-		getMarked()
-		eventSet = true;
-	end
 	Mark(self)
 end)
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("EQUIPMENT_SETS_CHANGED")
-frame:SetScript("OnEvent", function(self, event, ...)getMarked()
-CloseAllBags() 
-OpenAllBags()
+frame:SetScript("OnEvent", function(self, event, ...)
+	CloseAllBags() 
+	OpenAllBags()
 end)
 
 function Mark(self)
+	getMarked(self)
 	for i=1,self.size do
 		button=_G[self:GetName().."Item"..i]
-		slot = button:GetID()
-		itemId = GetContainerItemLink(self:GetID(), slot)
-		id = GetContainerItemID(self:GetID(),slot)
-		id = tostring(id)
-		
-		if ids[id] then
+		slotID = button:GetID()
+		bagID = self:GetID()
+		if spot[bagID][slotID] then
 			if button then
-			  button:SetHighlightTexture(icons[id])
+			  button:SetHighlightTexture(icons[bagID][slotID])
 			  button.IconBorder:SetVertexColor(1,0,0)
 			end
 		else
@@ -37,17 +31,29 @@ function Mark(self)
 end
 
 function getMarked()
-	ids = {}
 	icons = {}
+	spot = {}
 	count = C_EquipmentSet.GetNumEquipmentSets();
+	
+	for x=0,33 do
+		spot[x] = {}
+		icons[x] = {}
+	end
+	
 	for i = 0,count-1,1 do
 		name, texture = C_EquipmentSet.GetEquipmentSetInfo(i)
+		equipmentSetID = C_EquipmentSet.GetEquipmentSetID(name);
+		gear_locations = C_EquipmentSet.GetItemLocations(equipmentSetID);
 		itemIDs = C_EquipmentSet.GetItemIDs(i)
-		for i=1, 19 do
-		  if itemIDs[i] then
-			ids[tostring(itemIDs[i])] = itemIDs[i];
-			icons[tostring(itemIDs[i])] = texture;
-		  end
+	
+		for _, location in pairs(gear_locations) do
+			if(type(location) ~= "string") then
+				_, _, bags, _, slotIndex, bagIndex = EquipmentManager_UnpackLocation(location);
+				if(bags and (slotIndex and bagIndex)) then
+					spot[bagIndex][slotIndex] = true
+					icons[bagIndex][slotIndex] = texture;
+				end
+			end
 		end
 	end
 end
